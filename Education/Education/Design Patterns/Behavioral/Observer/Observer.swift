df76308a23d3b2a37d7ea48bd6d5883da77a5702
Sergii Stadnyk <sergey.stadnik.ios@gmail.com>
2020-11-18 18:46:12 +0200

    - Observer pattern implemented

--- Education/Education/Design Patterns/Behavioral/Observer/Observer.swift
+++ Education/Education/Design Patterns/Behavioral/Observer/Observer.swift
@@ -6,3 +6,30 @@
 //
 
 import Foundation
+
+protocol Observer: class {
+    func didReceiveNotification(_ manager: NotificationsManager)
+}
+
+class NotificationsManager {
+    
+    private var observers = [Observer]()
+    
+    func addObserver(_ observer: Observer) {
+        observers.append(observer)
+    }
+    
+    func removeObserver(_ observer: Observer) {
+        let indexOfElement = observers.firstIndex(where: { $0 === observer})
+        
+        if let index = indexOfElement {
+            observers.remove(at: index)
+        }
+    }
+    
+    func pushEvent() {
+        observers.forEach { (observer) in
+            observer.didReceiveNotification(self)
+        }
+    }
+}
