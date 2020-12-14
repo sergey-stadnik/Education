94b0fbe817c03359d84d66892211c5347a81b78a
Sergii Stadnyk <sergey.stadnik.ios@gmail.com>
2020-12-14 17:24:51 +0200

    Visitor pattern implemented

--- Education/Education/Design Patterns/Behavioral/Observer/Observer.swift
+++ Education/Education/Design Patterns/Behavioral/Observer/Observer.swift
@@ -5,6 +5,12 @@
 //  Created by Sergii Stadnyk on 11/18/20.
 //
 
+/*
+    EN: Observer is a publish/subscribe pattern which allows a number of observer objects to see an event.
+ 
+    RU: Определяет зависимость типа «один ко многим» между объектами таким образом, что при изменении состояния одного объекта все зависящие от него оповещаются об этом событии.
+*/
+
 import Foundation
 
 protocol Observer: class {
--- Education/Education/Design Patterns/Behavioral/Visitor/Visitor.swift
+++ Education/Education/Design Patterns/Behavioral/Visitor/Visitor.swift
@@ -5,4 +5,69 @@
 //  Created by Sergii Stadnyk on 11/18/20.
 //
 
+/*
+    EN: Visitor separates an algorithm from an object structure by moving the hierarchy of methods into one object.
+ 
+    RU: Позволяет добавлять в программу новые операции не изменяя классы обьектов, над которыми эти операции могут выполнятся.
+  
+*/
+
 import Foundation
+
+protocol PlanetVisitor {
+    func visit(planet: Venus)
+    func visit(planet: Mars)
+    func visit(planet: Saturn)
+}
+
+protocol Planet {
+    func accept(visitor: PlanetVisitor)
+}
+
+class Venus: Planet {
+    func accept(visitor: PlanetVisitor) {
+        visitor.visit(planet: self)
+    }
+}
+
+class Mars: Planet {
+    func accept(visitor: PlanetVisitor) {
+        visitor.visit(planet: self)
+    }
+}
+
+class Saturn: Planet {
+    func accept(visitor: PlanetVisitor) {
+        visitor.visit(planet: self)
+    }
+}
+
+
+
+class NameVisitor: PlanetVisitor {
+    var name: String = .init()
+
+    func visit(planet: Venus)  {
+        name = String(describing: Venus.self)
+    }
+    
+    func visit(planet: Mars) {
+        name = String(describing: Mars.self)
+    }
+    
+    func visit(planet: Saturn)  {
+        name = String(describing: Saturn.self)
+    }
+}
+
+/*
+ let planets: [Planet] = [Venus(), Mars(), Saturn()]
+
+ let visitor = NameVisitor()
+ 
+ for planet in planets {
+     planet.accept(visitor: visitor)
+     
+     print(visitor.name)
+ }
+ */
